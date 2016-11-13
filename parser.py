f4099e284d7de51438d45adc86eaf66ac58e6fcb
Rahul Agarwal <sjnider@gmail.com>
2016-11-14 01:27:12 +0530

    fetching json data

---
+++ parser.py
@@ -0,0 +1,7 @@
+import requests,json
+
+
+r= requests.get("https://gist.githubusercontent.com/murtuzakz/4bd887712703ff14c9b0f7c18229b332/raw/d0dd1c59016e2488dcbe0c8e710a1c5df9c3672e/season7.json")
+data = json.loads(r.text)
+
+print (data["Episode 1"][0]["status"])
